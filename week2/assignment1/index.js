0eae01a8ef7d07895509f0b9b5337fe316f8690c
Doyun Lee <doyn511@ajou.ac.kr>
2023-11-14 16:51:55 +0900

    [fix] assignment1 class명 상수화

--- week2/assignment1/index.js
+++ week2/assignment1/index.js
@@ -1,14 +1,16 @@
 const contentList = document.querySelectorAll(".img_hover_wrapper");
+const HIDDEN_CLASS = "hidden";
+const BG_BRIGHTNESS = "bg_brightness";
 
 contentList.forEach((content) => {
   const selectedImg = content.querySelector("img");
   const selectedEx = content.querySelector(".explanation");
   content.addEventListener("mouseover", function () {
-    selectedEx.classList.remove("hidden");
-    selectedImg.classList.add("bg_brightness");
+    selectedEx.classList.remove("HIDDEN_CLASS");
+    selectedImg.classList.add("BG_BRIGHTNESS");
   });
   content.addEventListener("mouseleave", function () {
-    selectedImg.classList.remove("bg_brightness");
-    selectedEx.classList.add("hidden");
+    selectedImg.classList.remove("BG_BRIGHTNESS");
+    selectedEx.classList.add("HIDDEN_CLASS");
   });
 });
