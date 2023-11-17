3f1d999ee31b36e396e28aba2e89cb75a8820cb7
Doyun Lee <doyn511@ajou.ac.kr>
2023-11-17 17:43:43 +0900

    [feat] 페이지 별 signup 버튼 수정

--- week4/assignment/src/components/common/signupBtn.jsx
+++ week4/assignment/src/components/common/signupBtn.jsx
@@ -1,17 +1,42 @@
 import styled from "styled-components";
 import { Btn } from "../../styles/CommonStyle";
+import { isCancel } from "axios";
 
-const SignUpBtn = ({ onClick }) => {
+const SignUpBtn = ({
+  onClick,
+  disabled,
+  isLoginPage,
+  isSignUpPage,
+  isActive,
+}) => {
   return (
     <>
-      <Button onClick={onClick}>회원가입</Button>
+      {isLoginPage && (
+        <InLoginPageBtn onClick={onClick}>회원가입</InLoginPageBtn>
+      )}
+      {isSignUpPage && !isActive && (
+        <InSignUpPageBtn onClick={onClick}>회원가입</InSignUpPageBtn>
+      )}
+      {isSignUpPage && isActive && (
+        <ActiveSignUpBtn onClick={onClick}>회원가입</ActiveSignUpBtn>
+      )}
     </>
   );
   // login페이지에서, disable일 때, enable일 때
 };
 export default SignUpBtn;
 
-const Button = styled(Btn)`
+const InLoginPageBtn = styled(Btn)`
   background-color: #b2b2b2;
+  color: #ffffff;
+`;
+
+const InSignUpPageBtn = styled(Btn)`
+  background-color: #cccccc;
+  cursor: auto;
+`;
+
+const ActiveSignUpBtn = styled(Btn)`
+  background-color: #444444;
   color: #ffffff;
 `;
