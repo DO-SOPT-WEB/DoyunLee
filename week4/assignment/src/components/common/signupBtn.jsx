477e9a76d175fbc00d0baea1a08ac252c3e84baa
Doyun Lee <doyn511@ajou.ac.kr>
2023-11-17 13:26:27 +0900

    [feat] signupBtn props 추가

--- week4/assignment/src/components/common/signupBtn.jsx
+++ week4/assignment/src/components/common/signupBtn.jsx
@@ -1,14 +1,17 @@
 import styled from "styled-components";
 import { Btn } from "../../styles/CommonStyle";
 
-const SignUpBtn = () => {
+const SignUpBtn = ({ onClick }) => {
   return (
     <>
-      <Button>회원가입</Button>
+      <Button onClick={onClick}>회원가입</Button>
     </>
   );
   // login페이지에서, disable일 때, enable일 때
 };
 export default SignUpBtn;
 
-const Button = styled(Btn)``;
+const Button = styled(Btn)`
+  background-color: #b2b2b2;
+  color: #ffffff;
+`;
